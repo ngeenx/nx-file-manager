6694cf5b88c6345070706d1dd5ae2c565ad5d661
Eyüp Erdoğan <epsilonelittr@gmail.com>
2025-01-31 15:02:35 +0300

    Add sub tree scroll height with props

--- libs/features/nx-angular-file-manager/src/lib/nx-angular-file-manager/components/folder-tree-fluid/folder-tree-fluid.component.ts
+++ libs/features/nx-angular-file-manager/src/lib/nx-angular-file-manager/components/folder-tree-fluid/folder-tree-fluid.component.ts
@@ -2,16 +2,20 @@ import { Component, Input } from "@angular/core";
 import { IFile } from "@ngeenx/nx-file-manager-utils";
 import { TreeItemComponent } from "./tree-item/tree-item.component";
 import { ChevronLeft, ChevronRight, LucideAngularModule } from "lucide-angular";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "nx-fm-folder-tree-fluid",
   templateUrl: "./folder-tree-fluid.component.html",
   standalone: true,
-  imports: [TreeItemComponent, LucideAngularModule],
+  imports: [TreeItemComponent, LucideAngularModule, CommonModule],
 })
 export class FolderTreeFluidComponent {
   @Input()
   public files!: IFile[];
+
+  @Input()
+  public height = 300;
 
   public ChevronRight = ChevronRight;
   public ChevronLeft = ChevronLeft;
