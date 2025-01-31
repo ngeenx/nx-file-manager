4a3f8d878a75231795f43ab992ac6c2fe18de94a
Eyüp Erdoğan <epsilonelittr@gmail.com>
2025-01-31 13:13:30 +0300

    Update default prefix

--- libs/features/nx-angular-file-manager/eslint.config.js
+++ libs/features/nx-angular-file-manager/eslint.config.js
@@ -1,44 +1,44 @@
-const nx = require('@nx/eslint-plugin');
-const baseConfig = require('../../../eslint.config.js');
+const nx = require("@nx/eslint-plugin");
+const baseConfig = require("../../../eslint.config.js");
 
 module.exports = [
   ...baseConfig,
   {
-    files: ['**/*.json'],
+    files: ["**/*.json"],
     rules: {
-      '@nx/dependency-checks': [
-        'error',
-        { ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'] },
+      "@nx/dependency-checks": [
+        "error",
+        { ignoredFiles: ["{projectRoot}/eslint.config.{js,cjs,mjs}"] },
       ],
     },
-    languageOptions: { parser: require('jsonc-eslint-parser') },
+    languageOptions: { parser: require("jsonc-eslint-parser") },
   },
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
+  ...nx.configs["flat/angular"],
+  ...nx.configs["flat/angular-template"],
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
+      "@angular-eslint/directive-selector": [
+        "error",
         {
-          type: 'attribute',
-          prefix: 'nx-angular',
-          style: 'camelCase',
+          type: "attribute",
+          prefix: "nx-fm",
+          style: "camelCase",
         },
       ],
-      '@angular-eslint/component-selector': [
-        'error',
+      "@angular-eslint/component-selector": [
+        "error",
         {
-          type: 'element',
-          prefix: 'nx-angular',
-          style: 'kebab-case',
+          type: "element",
+          prefix: "nx-fm",
+          style: "kebab-case",
         },
       ],
-      '@nx/enforce-module-boundaries': ['warn', {}],
+      "@nx/enforce-module-boundaries": ["warn", {}],
     },
   },
   {
-    files: ['**/*.html'],
+    files: ["**/*.html"],
     // Override or add rules here
     rules: {},
   },
